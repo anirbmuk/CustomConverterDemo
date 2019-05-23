674a5978a61c89cb84ecdf8ad3f0e6abad4a8983
anirbmuk <anirbmuk@live.in>
2019-05-23 13:58:03 +0530

    Refractored code

--- src/js/appController.js
+++ src/js/appController.js
@@ -7,20 +7,19 @@ define(['ojs/ojcore',
         'ojs/ojlabel'],
     function (oj, ko, ValidationBase) {
         function ControllerViewModel() {
-            var self = this;
+            const self = this;
+            
+            self.value = ko.observable();
+            self.phoneNumberConverter = ValidationBase
+                                              .Validation
+                                              .converterFactory("phonenumber")
+                                              .createCustomConverter();
 
             var smQuery = oj.ResponsiveUtils.getFrameworkQuery(oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY);
             self.smScreen = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(smQuery);
 
             self.appName = ko.observable("JET Custom Converter");
             self.userLogin = ko.observable("anirbmuk");
-            
-            self.value = ko.observable();
-            
-            self.phoneNumberConverter = ValidationBase
-                                              .Validation
-                                              .converterFactory("phonenumber")
-                                              .createCustomConverter();
 
         }
 
